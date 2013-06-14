c822560d324563417f5f33ed7b961cd73d566269
ikasama <ikasama@www25139ue.sakura.ne.jp>
2013-06-14 19:23:43 +0900

    change model.php

--- model.php
+++ model.php
@@ -4,8 +4,8 @@ function connect()
 {
     $pdo = new PDO(
         'mysql:host=localhost;dbname=sns;charset=utf8',
-        'root',
-        'l48A1pEB',
+        '',
+        '',
         array(PDO::ATTR_EMULATE_PREPARES => false)
     );
     return $pdo;
